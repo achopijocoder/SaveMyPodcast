38f3cce06c6f65ae54d089aeeeef7bdb50b91816
achopijocoder <achopijocoder@gmail.com>
2017-10-18 00:05:05 +0200

    Añade log4net a la solucion para facilitar diagnostico

--- SaveMyPodcast/Program.cs
+++ SaveMyPodcast/Program.cs
@@ -6,13 +6,16 @@ using System.IO;
 using NAudio.Wave;
 
 namespace HttpListenerWebSocketEcho
-{         
-    class Program
+{
+    public class Program
     {
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
             var server = new Server();
             server.Start("http://localhost:11000/");
+            log.Info("Servidor arrancó...");
             Console.WriteLine("Pulsa cualquier tecla para salir ...");
             Console.ReadKey();
         }
@@ -32,7 +35,7 @@ namespace HttpListenerWebSocketEcho
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add(listenerPrefix);
             listener.Start();
-            Console.WriteLine("Listening...");
+            Program.log.Info("Servidor escuchando...");
 
             while (true)
             {
@@ -62,12 +65,14 @@ namespace HttpListenerWebSocketEcho
                 webSocketContext = await listenerContext.AcceptWebSocketAsync(subProtocol: null);
                 Interlocked.Increment(ref count);
                 Console.WriteLine("Recibidos: {0}", count);
+                Program.log.Info("Conexiones recibidas: " + count.ToString());
             }
             catch (Exception e)
             {
                 listenerContext.Response.StatusCode = 500;
                 listenerContext.Response.Close();
                 Console.WriteLine("Exception: {0}", e);
+                Program.log.Error("Exception: " + e.Message);
                 return;
             }
 
@@ -80,6 +85,8 @@ namespace HttpListenerWebSocketEcho
 
                 //Especifica nombre del fichero a guardar
                 string fileName = String.Format("{0}_{1}.wav",count.ToString(), DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+                Program.log.Info("Generando archivo..."+fileName);
 
                 //crea el fichero donde se irán escribiendo los bytes recibidos
                 var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
@@ -96,6 +103,8 @@ namespace HttpListenerWebSocketEcho
                     if (receiveResult.MessageType == WebSocketMessageType.Close)
                     {
                         await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+
+                        Program.log.Info("Se terminó de recibir el archivo..." + fileName);
 
                         //se cierra el stream del fichero
                         fs.Close();
@@ -117,6 +126,7 @@ namespace HttpListenerWebSocketEcho
             catch (Exception e)
             {
                 Console.WriteLine("Exception: {0}", e);
+                Program.log.Fatal("Exception: " + e.Message);
             }
             finally
             {
